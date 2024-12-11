dfb264cb5e5bc4b1cbace595f252113b908bfb6e
CheBeomHee <tigerfrom2@naver.com>
2024-12-11 13:36:28 +0900

    git action 연습

---
+++ src/main/java/resume/cicdprac/CicdpracApplication.java
@@ -0,0 +1,13 @@
+package resume.cicdprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdpracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdpracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/resume/cicdprac/CicdpracApplicationTests.java
@@ -0,0 +1,13 @@
+package resume.cicdprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdpracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

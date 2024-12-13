a8199619122b3c54b51024fbacb0c4c49d5d4caf
CheBeomHee <tigerfrom2@naver.com>
2024-12-13 15:33:18 +0900

    fix: using port page implement

--- src/main/java/resume/cicdprac/User.java
+++
@@ -1,4 +0,0 @@
-package resume.cicdprac;
-
-public class User {
-}
---
+++ src/main/java/resume/cicdprac/contorller/PageController.java
@@ -0,0 +1,21 @@
+package resume.cicdprac.contorller;
+
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PageController {
+    private final Environment environment;
+
+    public PageController(Environment environment) {
+        this.environment = environment;
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        String port = environment.getProperty("local.server.port");
+        return "Currently running on port: " + port;
+    }
+}
